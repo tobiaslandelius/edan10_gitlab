28b74bbbd689d5d249f3b5a95bb25d9184e03fca
tobiaslandelius <tlandelius@gmail.com>
2016-12-02 09:21:44 +0100

    Experimenting 1

--- src1/project/src/MyTree.java
+++ src1/project/src/MyTree.java
@@ -30,7 +30,7 @@ public class MyTree {
   /**
    * Inserts a node in this tree. The tree is sorted by the node values.  If a
    * node with the same node value is already in the tree nothing happens.
-   *
+   * Tobbe experimenting on experimental branch
    * @param node The node to insert in this tree.
    * @return True if node is inserted, false otherwise.
    */
